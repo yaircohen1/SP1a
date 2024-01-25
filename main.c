2314abea64bb882f0d0ed0794ebea8816c797547
Yair Cohen <coheny405@gmail.com>
2024-01-25 15:32:03 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,13 @@ int main() {
     int num1, num2;
     scanf("%d", &num1);
     scanf("%d", &num2);
+    
+    if (num1 > num2){
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
+    
     printf("The Armstrong numbers are:");
     for (int i = num1; i <= num2; i++) {
         if (isArmstrong(i)) {
